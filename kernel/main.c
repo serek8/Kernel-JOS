09f85216fa2cdc5e2b322818875ab6e72ab3c9ae
Jan Seredynski <janseredynski@gmail.com>
2019-09-04 00:36:16 +0200

    boot_alloc & mem_init

--- kernel/main.c
+++ kernel/main.c
@@ -11,7 +11,6 @@
 void kmain(struct boot_info *boot_info)
 {
 	extern char edata[], end[];
-
 	/* Before doing anything else, complete the ELF loading process.
 	 * Clear the uninitialized global data (BSS) section of our program.
 	 * This ensures that all static/global variables start out zero.
