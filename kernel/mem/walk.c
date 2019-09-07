88e2bd56120bb22a00ab543abfe6e44be5cf8f5e
jonastheis <work@jonastheis.de>
2019-09-07 11:43:48 +0200

    Implement pdpt_walk_range, pdir_walk_range, ptbl_walk_range

--- kernel/mem/walk.c
+++ kernel/mem/walk.c
@@ -48,6 +48,28 @@ static int ptbl_walk_range(struct page_table *ptbl, uintptr_t base,
     uintptr_t end, struct page_walker *walker)
 {
 	/* LAB 2: your code here. */
+	physaddr_t entry;
+	uintptr_t next = base;
+	int next_index = PAGE_TABLE_INDEX(next);
+	uintptr_t next_end = ptbl_end(next);
+
+	while(next_index <= PAGE_TABLE_INDEX(end)) {
+		cprintf("ptbl_walk_range, next_index=%d, next=%p, base=%p, end=%p\n", next_index, next, base, end);
+		entry = ptbl->entries[next_index];
+
+		if(walker->get_pte) walker->get_pte(&entry, next, next_end, walker);
+
+		if((entry & PAGE_PRESENT) == 1) {
+			if(walker->unmap_pte) walker->unmap_pte(&entry, next, next_end, walker);
+		} else {
+			if(walker->pte_hole) walker->pte_hole(next, next_end, walker);
+		}
+
+		if(next_index == 511) break;
+		next = ptbl_end(next) + 1;
+		next_index = PAGE_TABLE_INDEX(next);
+		next_end = ptbl_end(next);
+	}
 	return 0;
 }
 
@@ -66,6 +88,30 @@ static int pdir_walk_range(struct page_table *pdir, uintptr_t base,
     uintptr_t end, struct page_walker *walker)
 {
 	/* LAB 2: your code here. */
+	physaddr_t entry;
+	uintptr_t next = base;
+	int next_index = PAGE_DIR_INDEX(next);
+	uintptr_t next_end = pdir_end(next);
+
+	while(next_index <= PAGE_DIR_INDEX(end)) {
+		cprintf("pdir_walk_range, next_index=%d, next=%p, base=%p, end=%p\n", next_index, next, base, end);
+		entry = pdir->entries[next_index];
+
+		if(walker->get_pde) walker->get_pde(&entry, next, next_end, walker);
+
+		if((entry & PAGE_PRESENT) == 1) {
+			// TODO: consider huge page
+			ptbl_walk_range((struct page_table*)PAGE_ADDR(entry), next, next_end, walker);
+			if(walker->unmap_pde) walker->unmap_pde(&entry, next, next_end, walker);
+		} else {
+			if(walker->pte_hole) walker->pte_hole(next, next_end, walker);
+		}
+
+		if(next_index == 511) break;
+		next = pdir_end(next) + 1;
+		next_index = PAGE_DIR_INDEX(next);
+		next_end = pdir_end(next);
+	}
 	return 0;
 }
 
@@ -84,6 +130,32 @@ static int pdpt_walk_range(struct page_table *pdpt, uintptr_t base,
     uintptr_t end, struct page_walker *walker)
 {
 	/* LAB 2: your code here. */
+	physaddr_t entry;
+	uintptr_t next = base;
+	int next_index = PDPT_INDEX(next);
+	uintptr_t next_end = pdpt_end(next);
+
+	while(next_index <= PDPT_INDEX(end)) {
+		cprintf("pdpt_walk_range, next_index=%d, next=%p, base=%p, end=%p\n", next_index, next, base, end);
+		entry = pdpt->entries[next_index];
+
+		if(walker->get_pdpte) walker->get_pdpte(&entry, next, next_end, walker);
+
+		if((entry & PAGE_PRESENT) == 1) {
+			cprintf("%p-%p\n", next, next_end);
+			// TODO: consider huge page
+			pdir_walk_range((struct page_table*)PAGE_ADDR(entry), next, next_end, walker);
+			if(walker->unmap_pdpte) walker->unmap_pdpte(&entry, next, next_end, walker);
+		} else {
+			if(walker->pte_hole) walker->pte_hole(next, next_end, walker);
+		}
+
+		if(next_index == 511) break;
+		next = pdpt_end(next) + 1;
+		next_index = PDPT_INDEX(next);
+		next_end = pdpt_end(next);
+	}
+
 	return 0;
 }
 
@@ -106,8 +178,8 @@ static int pml4_walk_range(struct page_table *pml4, uintptr_t base, uintptr_t en
 	int next_index = PML4_INDEX(next);
 	uintptr_t next_end = pml4_end(next);
 
-	//while(next_index <= PML4_INDEX(end)) {
-	while(1) {
+	while(next_index <= PML4_INDEX(end)) {
+		cprintf("pml4_walk_range, next_index=%d, next=%p, base=%p, end=%p\n", next_index, next, base, end);
 		entry = pml4->entries[next_index];
 		
 		if(walker->get_pml4e) walker->get_pml4e(&entry, next, next_end, walker);
