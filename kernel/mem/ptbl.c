b48ea65c62d922d9e0232fd683b17281c65348e0
Jan Seredynski <janseredynski@gmail.com>
2019-09-07 17:21:31 +0200

    remove

--- kernel/mem/ptbl.c
+++ kernel/mem/ptbl.c
@@ -82,5 +82,17 @@ int ptbl_free(physaddr_t *entry, uintptr_t base, uintptr_t end,
     struct page_walker *walker)
 {
 	/* LAB 2: your code here. */
+	if(!(*entry & PAGE_PRESENT)){
+		return -1; // no page table is present
+	}
+
+	struct page_table *pt = (struct page_table*)KADDR((PAGE_ADDR(entry)));
+	for(uint64_t i=0; i<PAGE_TABLE_ENTRIES; i++){
+		if(pt->entries[i] & PAGE_PRESENT){
+			return 0;
+		}
+	}
+	struct page_info *page = pa2page(*entry);
+	page_decref(page);
 	return 0;
 }
