4979f11c2c6b69e8fe6f8873501d4bc3005c5b6a
Jan Seredynski <janseredynski@gmail.com>
2019-09-08 18:38:28 +0200

    cr3 works until monitor

--- kernel/mem/insert.c
+++ kernel/mem/insert.c
@@ -29,6 +29,7 @@ static int insert_pte(physaddr_t *entry, uintptr_t base, uintptr_t end,
 	page = info->page;
 	page->pp_ref++;
 	*entry = info->flags | PAGE_ADDR(page2pa(page));
+	cprintf("insert_pte: va=%p ==> pa=%p\n", (void*)base, page2pa(page));
 
 	return 0;
 }
