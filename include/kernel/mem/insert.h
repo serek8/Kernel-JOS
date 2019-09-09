82047d44f82a1cd31edaaf863c401070eb83c17e
Jan Seredynski <janseredynski@gmail.com>
2019-09-10 01:09:59 +0200

    TLB done

--- include/kernel/mem/insert.h
+++ include/kernel/mem/insert.h
@@ -3,6 +3,12 @@
 #include <types.h>
 #include <paging.h>
 
+struct insert_info {
+	struct page_table *pml4;
+	struct page_info *page;
+	uint64_t flags;
+};
+
 int page_insert(struct page_table *pml4, struct page_info *page, void *va,
     uint64_t flags);
 
