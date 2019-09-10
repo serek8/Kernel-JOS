ae8a96d80136cdfb83f6c5aa77ccbdb61b234055
Jan Seredynski <janseredynski@gmail.com>
2019-09-10 14:28:16 +0200

    boot_map: added TLB and fixed if-stmts

--- include/kernel/mem/map.h
+++ include/kernel/mem/map.h
@@ -4,6 +4,13 @@
 #include <elf.h>
 #include <paging.h>
 
+struct boot_map_info {
+	struct page_table *pml4;
+	uint64_t flags;
+	physaddr_t pa;
+	uintptr_t base, end;
+};
+
 void boot_map_region(struct page_table *pml4, void *va, size_t size,
     physaddr_t pa, uint64_t flags);
 void *mmio_map_region(physaddr_t pa, size_t size);
