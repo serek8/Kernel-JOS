d91a07ec1bad563b7341e105c7da09ad1c711636
jonastheis <work@jonastheis.de>
2019-09-20 18:33:42 +0200

    Add basic validation for page faults to not always try to populate physical pages

--- kernel/vma/pfault.c
+++ kernel/vma/pfault.c
@@ -9,6 +9,11 @@ int task_page_fault_handler(struct task *task, void *va, int flags)
 {
 	/* LAB 4: your code here. */
 	//TODO: check access rights
+	struct vma *vma = find_vma(NULL, NULL, &task->task_rb, va);
+	if(!(vma->vm_base <= va && vma->vm_end >= va)) {
+		return -1;
+	}
+
 	void *page_start = ROUNDDOWN(va, PAGE_SIZE);
 	return populate_vma_range(task, page_start, PAGE_SIZE, flags); // TODO: what if its HUGEPAGE
 }
