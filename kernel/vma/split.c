a9a5b8868aadc6ca5b3a5d81d75a0b8fb8a335c6
Jan Seredynski <janseredynski@gmail.com>
2019-09-21 16:45:56 +0200

    fix for right split

--- kernel/vma/split.c
+++ kernel/vma/split.c
@@ -8,18 +8,18 @@
  * by setting the end address of original VMA to the given address and by
  * adding a new VMA with the given address as base.
  */
-struct vma *split_vma(struct task *task, struct vma *lhs, void *addr) // returns  [  | X ] 
+struct vma *split_vma(struct task *task, struct vma *lhs, void *addr) // returns  [  | ret ] 
 {
 	/* LAB 4: your code here. */
-	struct vma *rha = add_anonymous_vma(
+	struct vma *rhs = add_anonymous_vma(
 			task, lhs->vm_name,//, 
 			addr, 
 			lhs->vm_end - addr, 
 			lhs->vm_flags);
 	
-	rha->vm_src += (addr - lhs->vm_base) - rha->base_offset;
+	rhs->vm_src += (addr - lhs->vm_base) - rhs->base_offset;
 	lhs->vm_end = addr;
-	return rha;
+	return rhs;
 }
 
 /* Given a task and a VMA, this function first splits the VMA into a left-hand
@@ -28,16 +28,13 @@ struct vma *split_vma(struct task *task, struct vma *lhs, void *addr) // returns
  * and a right-hand side. This function finally returns the right-hand side of
  * the first split or the original VMA.
  */
-struct vma *split_vmas(struct task *task, struct vma *vma, void *base, size_t size) // returns  [ |X| ] or [ | X ]
+struct vma *split_vmas(struct task *task, struct vma *vma, void *base, size_t size) // returns  [  |ret|  ] 
 {
 	/* LAB 4: your code here. */
-	struct vma *cmva = NULL;
-	if(vma->vm_base != base){
-		cmva = split_vma(task, vma, base);     // [  vma  | cmva ]
-	} else{
-		cmva = vma;                            // [  vma = cmva  ]
-	}
-	struct vma *rmva = split_vma(task, cmva, base+size);
-	return cmva;
+	struct vma *middle_vma = split_vma(task, vma, base);
+	struct vma *rhs = split_vma(task, middle_vma, base+size);
+	return middle_vma;
 }
 
+
+// cprintf("remove_vma: vma->vm_base=%p, vma->vm_end=%p, vma->flags=%p, name=%s\n", vma->vm_base, vma->vm_end, vma->vm_flags, vma->vm_name);
