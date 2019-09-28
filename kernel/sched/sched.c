9840c63f7fcd9a0fb88b158cf74c4a099775be6e
Jan Seredynski <janseredynski@gmail.com>
2019-09-28 13:09:35 +0200

    fix for scheduler

--- kernel/sched/sched.c
+++ kernel/sched/sched.c
@@ -22,11 +22,25 @@ void sched_init(void)
 void sched_yield(void)
 {
 	/* LAB 5: your code here. */
+	if(cur_task != NULL){ // check if the task is already in a queue. Otherwiese, we would push the task to the list even during normal syscalls.
+		list_push_left(&runq, &cur_task->task_node);
+		// cprintf("# pushed cur_task->task_pid=%d\n", cur_task->task_pid);
+	}
+
 	if(list_is_empty(&runq)){
 		cprintf("Destroyed the only task - nothing more to do!\n");
 		sched_halt();
 	}
+	// struct list *node;
+	// struct task *t;
+	// list_foreach(&runq, node) {
+	// 	t = container_of(node, struct task, task_node);
+	// 	cprintf("> list_foreach: task_pid=%d\n", t->task_pid);
+	// }
+		
+
 	struct task *next_task = container_of(list_pop_left(&runq), struct task, task_node);
+	// cprintf("# popped next_task->task_pid=%d\n", next_task->task_pid);
 	task_run(next_task);
 }
 
